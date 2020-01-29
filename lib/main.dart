3516c3b6154988c210e50e509f96d89f2bbd4a2e
kishore.i <kishore.ijjagiri@halcyontek.com>
2020-01-29 19:10:38 +0530

    organizing the code in flutter..

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,30 @@
 import 'package:flutter/material.dart';
 
-void main()=>runApp(
+void main()=>runApp(MyFlutterApp());
 
-  MaterialApp(
-      title: "my app title",
-      home:Scaffold(
-        appBar: AppBar(title: Text("my app"),),
-        body:  Material(
-          color: Colors.amber,
-          child:Center(
-            child: Text(
-              "hellowdd world",
-              textDirection: TextDirection.ltr,
-              style: TextStyle(color: Colors.white,fontSize: 40.0),
-            ),
-          )
-          ,),
-      )
 
-  )
+class MyFlutterApp extends StatelessWidget
+{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+        title: "my app title",
+        home:Scaffold(
+          appBar: AppBar(title: Text("my app"),),
+          body:  Material(
+            color: Colors.amber,
+            child:Center(
+              child: Text(
+                "hellowdd world",
+                textDirection: TextDirection.ltr,
+                style: TextStyle(color: Colors.white,fontSize: 40.0),
+              ),
+            )
+            ,),
+        )
 
-);
+    );
+  }
+
+}

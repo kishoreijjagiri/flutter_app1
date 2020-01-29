191bea2321298894001cfd06f63f4baa99b722ce
kishore.i <kishore.ijjagiri@halcyontek.com>
2020-01-29 19:03:02 +0530

    designig UI with dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,23 @@
 import 'package:flutter/material.dart';
 
 void main()=>runApp(
-Center(
-  child: Text(
-    "hellow world",
-    textDirection: TextDirection.ltr
-    ),
-   )
+
+  MaterialApp(
+      title: "my app title",
+      home:Scaffold(
+        appBar: AppBar(title: Text("my app"),),
+        body:  Material(
+          color: Colors.amber,
+          child:Center(
+            child: Text(
+              "hellowdd world",
+              textDirection: TextDirection.ltr,
+              style: TextStyle(color: Colors.white,fontSize: 40.0),
+            ),
+          )
+          ,),
+      )
+
+  )
+
 );
